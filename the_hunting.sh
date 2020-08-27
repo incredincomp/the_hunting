109d3ce6bfe92770a46987b4528ea9f06dfc168b
IncredInComp <incredincomp@gmail.com>
2020-08-27 16:57:46 -0700

    Update the_hunting.sh
    set base info

--- the_hunting.sh
+++ the_hunting.sh
@@ -1,1 +1,25 @@
-
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: the_hunting.sh
+#
+#        USAGE: ./the_hunting.sh
+#
+#   DESCRIPTION:
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: 
+#
+#          BUGS: 
+#         NOTES: v0.0.0
+#        AUTHOR: @incredincomp
+#  ORGANIZATION:
+#       CREATED: 08/27/2020 16:55:54
+#      REVISION: 00/00/2020 00:00:00
+#     LICENSING: 
+#===============================================================================
+clear
+set -o nounset                              # Treat unset variables as an error
+set -e
+# Uncomment to print script in console for debug
+#set -xv
