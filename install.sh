629b97bc174710320464812bd5c4d8ad4bad229e
ii <ii@br0k3n.thinkpad>
2020-08-30 14:17:09 -0700

    install amass, compile go and make gobuster binary

--- install.sh
+++ install.sh
@@ -4,8 +4,20 @@
 apt update && apt upgrade -y
 #prereqs
 apt install chromium -y
-apt install golang-go -y
+
+cd ./temp/
+wget https://golang.org/dl/go1.15.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.15.linux-amd64.tar.gz
+
+echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.bashrc
+source $HOME/.bashrc
+
 
 # tool install
 snap install amass
-apt install gobuster
+git clone https://github.com/OJ/gobuster
+cd ./gobuster/
+go get && go build
+make linux
+cd ./build/gobuster-linux-amd64
+sudo cp gobuster /usr/bin/gobuster
