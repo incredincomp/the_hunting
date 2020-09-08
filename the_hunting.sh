9c48b4280c3e79553c1b830c15e56ad40628ef94
IncredInComp <incredincomp@gmail.com>
2020-09-07 17:02:56 -0700

    testing

--- the_hunting.sh
+++ the_hunting.sh
@@ -61,9 +61,10 @@ fi
 
 target=""
 subdomain_scan_target=""
+excluded=""
 usage() { echo -e "Usage: ./the_hunting.sh -d <target domain> [-e] [excluded.domain.com,other.domain.com]\nOptions:\n  -e\t-\tspecify excluded subdomains\n " 1>&2; exit 1; }
 
-while getopts ":d:s:e:l:" o; do
+while getopts "d:s:el" o; do
     case "${o}" in
         d)
             target="$OPTARG"
