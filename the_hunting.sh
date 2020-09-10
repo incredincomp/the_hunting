faa0925a5569842e17b1c0eefd8fe537ca6afd7b
IncredInComp <incredincomp@gmail.com>
2020-09-09 23:56:00 -0700

    messing with getopts, thats probably what is it

--- the_hunting.sh
+++ the_hunting.sh
@@ -74,7 +74,7 @@ subdomain_scan_target=""
 declare -a excluded=()
 usage() { echo -e "Usage: ./the_hunting.sh -d <target domain> [-e] [excluded.domain.com,other.domain.com]\nOptions:\n  -e\t-\tspecify excluded subdomains\n " 1>&2; exit 1; }
 
-while getopts "d:s:el" o; do
+while getopts ":d:s:e:l" o; do
     case "${o}" in
         d)
             target="$OPTARG"
