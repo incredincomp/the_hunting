12a857158d46a6724b6466f33281628721171462
1efty <18194777+1efty@users.noreply.github.com>
2020-09-25 03:04:18 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,69 +1,34 @@
 #!/bin/bash -
 #Shouldnt need to be ran in axiom droplet
 #this is for install and use on ubuntu 20.04 for testing
-usage() { logo; echo -e "Usage: ./install.sh -[i]\nOptions:\n  -i\t-\tinstall everything\n  -u\t-\tupdate everything\n  -p\t-\tinstall pre_reqs\n  -t\t-\tinstall tools\n  -w\t-\tupdate wordlists\n  -a\t-\tupdate all tools\n  -l\t-\tless LICENSE\n" 1>&2; exit 1; }
-# niiiiice @1efty
-while [[ $1 ]]; do
-	echo "Handling [$1]..."
-	case "$1" in
-    --install)
-        install
-        ;;
-    --update_hunting)
-			  update_the_hunting
-		  	;;
-    --pre_reqs)
-        pre_reqs
-	  		;;
-    --install_tools)
-        install_tools
-  			;;
-    --update_wordlists)
-        update_wordlists
-	  		;;
-    --update_all)
-        update_all_tools
-	  		;;
-    --license)
-        less ./LICENSE
-        exit 1
-	  		;;
-    *)
-        echo "Error: Unknown option: $1" >&2
-	  		exit 1
-	  		;;
-	esac
-done
+usage() {
+  echo -e "Usage: ./install.sh -[i]\nOptions:\n  -i\t-\tinstall everything\n  -u\t-\tupdate everything\n  -p\t-\tinstall pre_reqs\n  -t\t-\tinstall tools\n  -w\t-\tupdate wordlists\n  -a\t-\tupdate all tools\n  -l\t-\tless LICENSE\n" 1>&2
+  exit 1
+}
 
-#shift $((OPTIND - 1))
-
-update_all_tools(){
-# Probably need to add some uname checks and then set up package repo.
+function update_all_tools() {
+  # Probably need to add some uname checks and then set up package repo.
   apt update && apt upgrade -y
   snap refresh
 }
-update_the_hunting(){
+
+function update_the_hunting() {
   git pull
 }
-#prereqs
-pre_reqs(){
-  snap install chromium
-  apt install parallel -y
-  go_lang
-}
 
-go_lang(){
-  wget https://golang.org/dl/go1.15.linux-amd64.tar.gz
-  sudo tar -C /usr/local -xzf go1.15.linux-amd64.tar.gz
-  echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.bashrc
-  source $HOME/.bashrc
+#prereqs
+function pre_reqs() {
+  apt update && apt upgrade -y
+  apt install snapd sudo wget git make unzip parallel golang -y
+  snap install chromium
 }
 
 # tool install
-install_amass(){
+function install_amass() {
   snap install amass
 }
-install_gobuster(){
+
+function install_gobuster() {
   git clone https://github.com/OJ/gobuster
   cd ./gobuster/
   go get && go build
@@ -71,31 +36,37 @@ install_gobuster(){
   cd ./build/gobuster-linux-amd64
   sudo cp gobuster /usr/bin/gobuster
 }
-install_subjack(){
+
+function install_subjack() {
   go get github.com/haccer/subjack
-  echo 'alias subjack="~/go/bin/subjack"' >> $HOME/.bashrc
+  echo 'alias subjack="~/go/bin/subjack"' >>$HOME/.bashrc
 }
-install_httprobe(){
+
+function install_httprobe() {
   go get -u github.com/tomnomnom/httprobe
   alias httprobe="~/go/bin/httprobe"
 }
-install_aquatone(){
+
+function install_aquatone() {
   wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip
   unzip aquatone_linux_amd64_1.7.0.zip
   mv aquatone /usr/local/bin/aquatone
 }
-install_nuclei(){
+
+function install_nuclei() {
   git clone https://github.com/projectdiscovery/nuclei.git
   cd nuclei/cmd/nuclei/
   go build .
   mv nuclei /usr/local/bin/
   cd ../../../
 }
-install_chromium(){
+
+function install_chromium() {
   # Probably need to add some uname checks and then set up package repo.
   type -P chromium &>/dev/null || sudo snap install chromium
 }
-install_tools(){
+
+function install_tools() {
   cd ./temp
   install_amass
   install_gobuster
@@ -107,8 +78,54 @@ install_tools(){
   cd ..
 }
 
-install(){
-  update_all_tools
+function install_all() {
   pre_reqs
   install_tools
 }
+
+function parse_args() {
+  # niiiiice @1efty
+  while [[ $1 ]]; do
+    echo "Handling [$1]..."
+    case "$1" in
+    --install)
+      install_all
+      shift
+      ;;
+    --update_hunting)
+      update_the_hunting
+      shift
+      ;;
+    --pre_reqs)
+      pre_reqs
+      shift
+      ;;
+    --install_tools)
+      install_tools
+      shift
+      ;;
+    --update_wordlists)
+      update_wordlists
+      shift
+      ;;
+    --update_all)
+      update_all_tools
+      shift
+      ;;
+    --license)
+      less ./LICENSE
+      exit 1
+      ;;
+    *)
+      echo "Error: Unknown option: $1" >&2
+      exit 1
+      ;;
+    esac
+  done
+}
+
+function main() {
+  parse_args $@
+}
+
+main $@
