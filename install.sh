55be09d15c7ac65af8dc4ddac6494f21fbdb7588
IncredInComp <incredincomp@gmail.com>
2020-09-26 18:08:52 -0700

    fixing jq download path

--- install.sh
+++ install.sh
@@ -121,7 +121,7 @@ function install_chromium() {
 
 function install_jq() {
   wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
-  mv /jq-linux64 /usr/local/bin/jq
+  mv ./jq-linux64 /usr/local/bin/jq
 }
 
 function install_tools() {
