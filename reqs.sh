c2c5ff35571bd6c58576042b7099e0527afb9fc4
IncredInComp <incredincomp@gmail.com>
2020-10-02 22:25:28 -0700

    way better awscli install and version

--- reqs.sh
+++ reqs.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-sudo apt install make python3-pip
-python3 -m pip install --user awscli
-export PATH="${PATH}:/root/.local/bin"
+sudo apt install make
+cd ./temp
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install -i /usr/local/aws-cli -b /usr/local/bin
+cd ..
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install packer
 echo "Set your aws configuration here, would you like to do this? [yn]"
 read answer
 if [ "$answer" == y ]; then
-  aws configure set region us-west-2
+  aws configure set region us-west-2 --profile
 fi
 echo "Do you need a new bucket to use? this may destroy data, beware! [yn]"
 read answer2
