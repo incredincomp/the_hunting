3c2b718288023340681af967243fc5955efa0651
IncredInComp <incredincomp@gmail.com>
2020-10-03 09:37:03 -0700

    changed to script provisioners

---
+++ build/scripts/finish.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+cp ~/backup-files/passwd-s3fs /etc/passwd-s3fs
+chmod 600 /etc/passwd-s3fs
---
+++ build/scripts/provision.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -e
+git clone --recurse-submodules https://github.com/incredincomp/the_hunting.git
+cd ./the_hunting
+./install.sh --install
