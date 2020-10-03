bd97f76ada5f86f2f0797b25a6549a584f0bb80a
IncredInComp <incredincomp@gmail.com>
2020-10-03 14:48:59 -0700

    setting service file variables

--- build/scripts/finish.sh
+++ build/scripts/finish.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -e
 cp ~/backup-files/passwd-s3fs /etc/passwd-s3fs
-chmod 600 /etc/passwd-s3fs
 cp ~/backup-files/* ~/the_hunting/backup-files/
+chmod 600 /etc/passwd-s3fs
+S3_ENDPOINT="$(cat /root/the_hunting/backup-files/s3-endpoint.txt)"
+S3_BUCKET="$(cat /root/the_hunting/backup-files/s3-bucket.txt)"
+sed -i 's+.*{TEXT_TO_BE_REPLACED}*+s3fs '"$S3_BUCKET"' /root/the_hunting/s3-booty -o nonempty -o url='"$S3_ENDPOINT"'+' ~/the_hunting/backup-files/s3fs.service
 cp ~/the_hunting/backup-files/s3fs.service /etc/systemd/system/s3fs.service
 chmod 664 /etc/systemd/system/s3fs.service
 systemctl daemon-reload
 systemctl enable s3fs.service
 apt update && apt upgrade -y
-echo "export S3_ENDPOINT=$(cat /root/the_hunting/backup-files/s3-endpoint.txt)" >> ~/.bashrc
-echo "export S3_BUCKET=$(cat /root/the_hunting/backup-files/s3-bucket.txt)" >> ~/.bashrc
-source .bashrc
