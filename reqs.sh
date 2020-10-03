046212171d1d990a75203a0fff70bb93fed28079
IncredInComp <incredincomp@gmail.com>
2020-10-02 22:33:58 -0700

    okay sure path

--- reqs.sh
+++ reqs.sh
@@ -5,6 +5,8 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install -i /usr/local/aws-cli -b /usr/local/bin
 cd ..
+export PATH="${PATH}:/usr/local/aws-cli/v2/current/bin"
+source ~/.bashrc
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install packer
