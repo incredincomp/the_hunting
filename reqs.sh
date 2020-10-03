69f3f59df9884f9e41c1800c2cc2723d95412366
IncredInComp <incredincomp@gmail.com>
2020-10-02 22:50:35 -0700

    profile

--- reqs.sh
+++ reqs.sh
@@ -7,9 +7,9 @@ else
   curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
   unzip awscliv2.zip
   sudo ./aws/install -i /usr/local/aws-cli -b /usr/local/bin
+  export PATH="${PATH}:/usr/local/aws-cli/v2/current/bin"
 fi
 cd ..
-export PATH="${PATH}:/usr/local/aws-cli/v2/current/bin"
 source ~/.bashrc
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
@@ -18,7 +18,7 @@ aws_config() {
   echo "Set your aws configuration here, would you like to do this? [yn]"
   read answer
   if [ "$answer" == y ]; then
-    aws configure set region us-west-2
+    aws configure --profile the_hunting
   fi
 }
 aws_create() {
