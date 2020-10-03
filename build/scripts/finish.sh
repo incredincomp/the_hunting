9c1f68e362d51ea76e390cbb397707cfcd805f6c
IncredInComp <incredincomp@gmail.com>
2020-10-03 15:03:47 -0700

    readded ExecStart= to fix systemd error

--- build/scripts/finish.sh
+++ build/scripts/finish.sh
@@ -4,7 +4,7 @@ cp ~/backup-files/* ~/the_hunting/backup-files/
 chmod 600 /etc/passwd-s3fs
 S3_ENDPOINT="$(cat /root/the_hunting/backup-files/s3-endpoint.txt)"
 S3_BUCKET="$(cat /root/the_hunting/backup-files/s3-bucket.txt)"
-sed -i 's+.*{TEXT_TO_BE_REPLACED}*+s3fs '"$S3_BUCKET"' /root/the_hunting/s3-booty -o nonempty -o url='"$S3_ENDPOINT"'+' ~/the_hunting/backup-files/s3fs.service
+sed -i 's+.*{TEXT_TO_BE_REPLACED}*+ExecStart=s3fs '"$S3_BUCKET"' /root/the_hunting/s3-booty -o nonempty -o url='"$S3_ENDPOINT"'+' ~/the_hunting/backup-files/s3fs.service
 cp ~/the_hunting/backup-files/s3fs.service /etc/systemd/system/s3fs.service
 chmod 664 /etc/systemd/system/s3fs.service
 systemctl daemon-reload
