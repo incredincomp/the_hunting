0c5771ab909157132f710c1b73d2f134e42416e7
IncredInComp <incredincomp@gmail.com>
2020-10-10 16:46:48 -0700

    who knows

--- the_hunting.sh
+++ the_hunting.sh
@@ -362,8 +362,8 @@ function tmux_image() {
   if [ -z "$sessions" ]; then
     ssh -o StrictHostKeyChecking=no root@"$image_ip" 'tmux new-session -t hunting' && ssh -o StrictHostKeyChecking=no -t root@"$image_ip" 'tmux attach -t hunting -d'
   else
-    tmux_session=$(echo "${sessions/:/}" | awk '/hunting/ {print $1}' | head -n1)
-    ssh -o StrictHostKeyChecking=no -t root@"$image_ip" 'tmux attach -t '"$tmux_session"' -d'
+    #tmux_session=$(echo "${sessions/:/}" | awk '/hunting/ {print $1}' | head -n1)
+    ssh -o StrictHostKeyChecking=no -t root@"$image_ip" 'tmux attach -t hunting-0 -d'
   fi
 }
 # S3fs-fuse
