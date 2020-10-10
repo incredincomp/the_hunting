9a92f8992328f4c0b3a0737b5b0e990d8f8b857d
IncredInComp <incredincomp@gmail.com>
2020-10-10 13:28:42 -0700

    tmux fix

--- the_hunting.sh
+++ the_hunting.sh
@@ -357,7 +357,8 @@ function remove_image() {
 function tmux_image() {
   #image_id=$(doctl compute image list | awk '/the_hunting/ {print $1}' | head -n1)
   image_ip=$(doctl compute droplet list --format "Name,PublicIPv4" | awk '/the-hunting/ {print $2}' | head -n1)
-  ssh root@"$image_ip" -t tmux attach-session hunting
+   op@"$ip" 'tmux new-session -d -t main'
+  ssh -o StrictHostKeyChecking=no root@"$image_ip" 'tmux new-session -d -t hunting'
 }
 # S3fs-fuse
 function upload_s3_recon() {
