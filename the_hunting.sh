cf079ac38572f0d7e7623c2a8ef990c89e1e566f
IncredInComp <incredincomp@gmail.com>
2020-10-10 15:41:57 -0700

    motd fix

--- the_hunting.sh
+++ the_hunting.sh
@@ -358,7 +358,7 @@ function remove_image() {
 function tmux_image() {
   #image_id=$(doctl compute image list | awk '/the_hunting/ {print $1}' | head -n1)
   image_ip=$(doctl compute droplet list --format "Name,PublicIPv4" | awk '/the-hunting/ {print $2}' | head -n1)
-  ssh -o StrictHostKeyChecking=no -t root@"$image_ip" 'tmux new-session -t hunting; ./root/bin/tmux-motd.sh'
+  ssh -o StrictHostKeyChecking=no -t root@"$image_ip" 'tmux new-session -t hunting && ./root/bin/tmux-motd.sh'
   #ssh -o StrictHostKeyChecking=no root@"$image_ip" 'tmux attach -t hunting -d'
 }
 # S3fs-fuse
