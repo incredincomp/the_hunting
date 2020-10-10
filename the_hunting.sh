2301bfb8c3c405029130f577225cd370c4e1110e
IncredInComp <incredincomp@gmail.com>
2020-10-10 16:04:22 -0700

    -c

--- the_hunting.sh
+++ the_hunting.sh
@@ -359,7 +359,7 @@ function tmux_image() {
   #image_id=$(doctl compute image list | awk '/the_hunting/ {print $1}' | head -n1)
   image_ip=$(doctl compute droplet list --format "Name,PublicIPv4" | awk '/the-hunting/ {print $2}' | head -n1)
 
-  ssh -o StrictHostKeyChecking=no -t root@"$image_ip" 'tmux new-session -t hunting; ./root/bin/tmux-motd.sh;'
+  ssh -o StrictHostKeyChecking=no -t root@"$image_ip" 'tmux new-session -t hunting -c ./root/bin/tmux-motd.sh;'
   #ssh -o StrictHostKeyChecking=no root@"$image_ip" 'tmux attach -t hunting -d'
 }
 # S3fs-fuse
