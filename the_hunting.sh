4cc4eee2b944003916b1a31e36ff4e96131f4819
IncredInComp <incredincomp@gmail.com>
2020-10-10 17:35:39 -0700

    checking

--- the_hunting.sh
+++ the_hunting.sh
@@ -358,8 +358,8 @@ function remove_image() {
 function tmux_image() {
   #image_id=$(doctl compute image list | awk '/the_hunting/ {print $1}' | head -n1)
   image_ip=$(doctl compute droplet list --format "Name,PublicIPv4" | awk '/the-hunting/ {print $2}' | head -n1)
-  response=$(ssh -o StrictHostKeyChecking=no root@"$image_ip" 'tmux list-session')
-  if [[ "$response" == "error connecting to /tmp/tmux-0/default (No such file or directory)" ]]; then
+  response=$(ssh -o StrictHostKeyChecking=no root@"$image_ip" 'tmux list-session' 2>&1)
+  if [ $? -eq 255 ]; then
     ssh -o StrictHostKeyChecking=no -t root@"$image_ip" 'tmux new-session -t hunting'
 #    ssh -o StrictHostKeyChecking=no -t root@"$image_ip" 'tmux attach -t hunting-0 -d'
   else
