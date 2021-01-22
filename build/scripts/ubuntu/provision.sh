80b0bd0e68988cbfe2760f1f7d4ea1eada265b24
IncredInComp <incredincomp@gmail.com>
2021-01-21 22:50:56 -0800

    install rust at make

--- build/scripts/ubuntu/provision.sh
+++ build/scripts/ubuntu/provision.sh
@@ -3,3 +3,4 @@ git clone https://github.com/incredincomp/the_hunting.git
 cd ./the_hunting
 git clone https://github.com/projectdiscovery/nuclei-templates.git
 ./files/conf/install.sh --install
+curl https://sh.rustup.rs -sSf | sh
