d6bbe1b78b25f33189e4eb95fd2ffd6924c32ba1
IncredInComp <incredincomp@gmail.com>
2021-01-21 23:22:34 -0800

    fix for sh -s -- -y

--- build/scripts/ubuntu/provision.sh
+++ build/scripts/ubuntu/provision.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-curl https://sh.rustup.rs -sSf | sh -y
+curl https://sh.rustup.rs -sSf | sh -s -- -y
 git clone https://github.com/incredincomp/the_hunting.git
 cd ./the_hunting
 git clone https://github.com/projectdiscovery/nuclei-templates.git
